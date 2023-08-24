4eb49c80697f3c441bae80618533498c12101c63
poiuy4004 <poiuy4004@naver.com>
2023-08-24 20:07:34 +0900

    Client start

---
+++ Client/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./component/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/component/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+function Header(){
+  return(
+    <header>
+      <i className="fa-solid fa-bars"></i>
+      <img src="./textLogo.png" width="7%" />
+      <span>로그인</span>
+    </header>
+  )
+}
+export default Header;
