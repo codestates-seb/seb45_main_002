0428750d951d91937bbedabb0bca8072cddeb7ad
poiuy4004 <poiuy4004@naver.com>
2023-09-01 11:07:41 +0900

    hamberger needs modal

--- Client/src/component/Main.js
+++ Client/src/component/Main.js
@@ -1,11 +1,13 @@
 import Home from "../page/Home";
 import MyPage from "../page/MyPage";
 import TableAdd from "../page/TableAdd";
+import PageSwitch from "./PageSwitch"
 
 import { Routes, Route } from "react-router-dom";
 import { styled } from "styled-components";
 
 import style from "../style/style";
+import { useState } from "react";
 
 const MainContainer = styled.main`
   position: absolute;
@@ -17,12 +19,14 @@ const MainContainer = styled.main`
 `;
 
 function Main() {
+  const [page, setPage] = useState("/")
   return (
     <MainContainer>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-        <Route path="/tableadd" element={<TableAdd />}></Route>
+        <Route path="/" element={<Home setPage={setPage} />}></Route>
+        <Route path="/mypage" element={<MyPage setPage={setPage} />}></Route>
+        <Route path="/tableadd" element={<TableAdd setPage={setPage} />}></Route>
+        <Route path="/pageswitch" element={<PageSwitch page={page} />}></Route>
       </Routes>
     </MainContainer>
   );
