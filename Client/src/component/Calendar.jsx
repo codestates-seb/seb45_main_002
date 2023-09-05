086fa7ddfde878f12198d64e8df43788c25389b2
사월 <april35592@gmail.com>
2023-09-05 22:36:50 +0900

    devFE-feat/home layout

--- Client/src/component/Calendar.jsx
+++ Client/src/component/Calendar.jsx
@@ -1,10 +1,16 @@
 import { styled } from "styled-components";
+import style from "../style/style";
 
 const StyleCalendar = styled.div`
-  width: calc(50% - 16px);
-  max-width: 584px;
-  aspect-ratio: 0.9;
-  background-color: #e6e6e6;
+  width: calc(100% - 16px);
+  max-width: 340px;
+  height: 280px;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: ${style.color.white};
+  border-radius: 8px;
 `;
 
 const Calendar = () => {
