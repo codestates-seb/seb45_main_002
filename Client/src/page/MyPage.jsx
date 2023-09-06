5e738613a330cd325fc0d9be6fc142a53cdb670e
poiuy4004 <poiuy4004@naver.com>
2023-09-06 19:58:02 +0900

    mypage reset

--- Client/src/page/MyPage.jsx
+++ Client/src/page/MyPage.jsx
@@ -1,10 +1,7 @@
-import Input from "../atom/Input";
-
 function MyPage() {
   return (
     <article>
       
-      <input type="text" value="이건 샘플" placeholder="이건 샘플" ></input>
     </article>
   );
 }
