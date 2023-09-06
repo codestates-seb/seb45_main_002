2f742f06445557da54e56c0aab7770844e6ab3ca
poiuy4004 <125282928+poiuy4004@users.noreply.github.com>
2023-09-06 10:59:24 +0900

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,15 +1,24 @@
 import Header from "./component/Header";
-import Nav from "./component/Nav";
 import Main from "./component/Main";
+import Nav from "./component/Nav";
+import Hamberger from "./atom/Hamberger"
+
+import { useState } from "react";
 
 import style from "./style/style";
 
 function App() {
+
+  const [page, setPage] = useState("")
+
+  const [menu, setMenu] = useState(false)
+  
   return (
     <div>
-      <Header />
-      {style.layout.maxWidth < 768 ? null : <Nav />}
-      <Main />
+      <Header menu={menu} setMenu={setMenu} />
+      {style.layout.maxWidth < 769 ? null : <Nav />}
+      {menu? <Hamberger setPage={setPage} menu={menu} setMenu={setMenu} /> : null}
+      <Main page={page} setPage={setPage} />
     </div>
   );
 }
