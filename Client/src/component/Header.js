e9fcb8c7d36a8f81febd619305eedec03daa0ce3
poiuy4004 <125282928+poiuy4004@users.noreply.github.com>
2023-09-06 10:59:55 +0900

    Update Header.js

--- Client/src/component/Header.js
+++ Client/src/component/Header.js
@@ -2,50 +2,68 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
 import style from "../style/style";
-import ModalComponent from "../atom/GlobalModal";
+import Modal from "../atom/GlobalModal";
+import ModalPortal from "../atom/ModalPortal";
 import LoginForm from "./LoginForm";
-import SignUpForm from "./SignUpForm";
+import SignUpForm from "./SignUpForm"
 import { useState } from "react";
-import ReactDOM from "react-dom";
+import useZustand from "../zustand/Store";
 
 const HeaderContainer = styled.header`
   display: flex;
+  position: fixed;
+  top: 0;
+  width: 100%;
   height: ${style.layout.header.height};
   justify-content: space-between;
   border: solid 1px orange;
-  font-size: 200%;
-  & > * {
+  font-size: ${style.layout.header.height*7/8};
+  &>*{
     display: flex;
-    padding: 0.5% 0;
   }
-  & > :first-child > * {
-    margin-left: calc(${style.layout.maxWidth}px / 20 / 2);
+  &>:first-child>*{
+    margin-left: ${style.layout.maxWidth/20/3}px;
   }
-  & > :last-child > * {
-    margin-right: calc(${style.layout.maxWidth}px / 20 / 2);
+  &>:last-child{
+    align-items: center;
+    margin-right: ${style.layout.maxWidth/20/3}px;
+    padding: 1% 0;
   }
-`;
+`
+
+const HambergerI = styled.i`
+  cursor: pointer;
+`
 
 const LoginButton = styled.button`
+  height: ${style.layout.header.height/2};
   border: none;
   background-color: orange;
-  padding: 0 1%;
+  margin-right: ${style.layout.maxWidth/20/3}px;
+  padding: 0 3%;
   color: white;
-  font-size: 50%;
+  font-size: ${style.layout.header.height/3};
   font-weight: bolder;
   white-space: nowrap;
-  width: ${style.layout.sideMargin / 3};
   cursor: pointer;
-`;
+`
 const SignUpButton = styled(LoginButton)`
   background-color: green;
-`;
+`
 
-function Header() {
+function Header({menu,setMenu}) {
+
+  const accessToken = useZustand(state=>state.accessToken)
+  
   const [isOpen, setIsOpen] = useState(false);
   const [content, setContent] = useState(null);
   const [footer, setFooter] = useState(null);
   const [header, setHeader] = useState(null);
+
+  function logoutButton(){
+    localStorage.removeItem("access_token")
+    window.location.reload()
+  }
 
   const handleOpenLoginModal = () => {
     setIsOpen(true);
@@ -53,7 +71,6 @@ function Header() {
     setFooter("login footer입니다");
     setContent(<LoginForm />);
   };
-
   const handleOpenSignUpModal = () => {
     setIsOpen(true);
     setHeader("signup header입니다");
@@ -61,46 +78,55 @@ function Header() {
     setContent(<SignUpForm />);
   };
 
-  const handleCloseModal = () => {
-    setIsOpen(false);
-    setContent(null);
-    setHeader(null);
-    setFooter(null);
-  };
-
-  const modalPortal =
-    isOpen &&
-    ReactDOM.createPortal(
-      <ModalComponent
-        isOpen={isOpen}
-        onClose={handleCloseModal}
-        header={header}
-        content={content}
-        footer={footer}
-      >
-        {content}
-      </ModalComponent>,
-      document.getElementById("modal-root")
-    );
-
   return (
-    <HeaderContainer id="modal-root">
+    <HeaderContainer>
       <span>
-        {style.layout.maxWidth < 980 ? (
-          <i className="fa-solid fa-bars"></i>
+        {style.layout.maxWidth < 769 ? (
+          <HambergerI className="fa-solid fa-bars" onClick={()=>setMenu(!menu)} />
         ) : null}
         <Link to="/">
           <img
-            width={style.layout.maxWidth < 980 ? "100%" : "200%"}
+            src="https://media.discordapp.net/attachments/483947972380327936/1144245243550638090/NutritionCoders-1.png?width=1498&height=1002"
+            height={style.layout.header.height*7/8}
             alt="logo"
           />
         </Link>
       </span>
-      <span>
-        {modalPortal}
-        <LoginButton onClick={handleOpenLoginModal}>로그인</LoginButton>
-        <SignUpButton onClick={handleOpenSignUpModal}>회원가입</SignUpButton>
-      </span>
+      {accessToken?
+        <span>
+          <img alt="My Page" src="image/profileimage.svg" height={style.layout.header.height-style.layout.narrowMargin.height}></img>
+          <SignUpButton onClick={logoutButton}>로그아웃</SignUpButton>
+        </span>
+        :
+        <span>
+          <LoginButton onClick={handleOpenLoginModal}>로그인</LoginButton>
+          <SignUpButton onClick={handleOpenSignUpModal}>회원가입</SignUpButton>
+          <ModalPortal>
+            <Modal
+              isOpen={isOpen}
+              content={content}
+              header={header}
+              footer={footer}
+              setContent={setContent}
+              setHeader={setHeader}
+              setFooter={setFooter}
+              setIsOpen={setIsOpen}
+            />
+          </ModalPortal>
+          <ModalPortal>
+        <Modal
+          isOpen={isOpen}
+          content={content}
+          header={header}
+          footer={footer}
+          setContent={setContent}
+          setHeader={setHeader}
+          setFooter={setFooter}
+          setIsOpen={setIsOpen}
+        />
+      </ModalPortal>
+        </span>
+      }
     </HeaderContainer>
   );
 }
