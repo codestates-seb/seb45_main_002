a937ddb45368d63da96f0bf1331b7009807009fb
poiuy4004 <poiuy4004@naver.com>
2023-09-06 19:56:47 +0900

    add input sample

--- Client/src/page/MyPage.jsx
+++ Client/src/page/MyPage.jsx
@@ -3,14 +3,7 @@ import Input from "../atom/Input";
 function MyPage() {
   return (
     <article>
-      <Input
-       type={"text"}
-       value={"광광우럭따"}
-       onChange={()=>console.log("광광")}
-       placeholder={"광광울었냐"}
-
-       styling={""}
-      />
+      
       <input type="text" value="이건 샘플" placeholder="이건 샘플" ></input>
     </article>
   );
