0f4b04db31181b15324614dade60c59e3e5f305b
amberjackman <feverhong@vivaldi.net>
2023-09-09 03:43:32 +0900

    mockdata temp

--- Client/src/component/Calendar/Calendar.jsx
+++ Client/src/component/Calendar/Calendar.jsx
@@ -1,11 +1,12 @@
 import { styled } from "styled-components";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import Modal from "../../atom/GlobalModal";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 // import StyledCalendar from "../style/CalendarStyle";
 import "moment/locale/ko";
+import axios from "axios";
 
 const CalendarContainer = styled.div`
   max-width: 768px;
@@ -46,11 +47,6 @@ const CustomDot = styled.div`
 `;
 
 const customToolbar = (toolbar) => {
-  const goToToday = () => {
-    toolbar.date.set(new Date());
-    toolbar.onNavigate("TODAY");
-  };
-
   const goToPrev = () => {
     toolbar.onNavigate("PREV");
   };
@@ -61,44 +57,116 @@ const customToolbar = (toolbar) => {
 
   return (
     <div className="rbc-toolbar">
-      <span className="rbc-btn-group">
-        <button type="button" onClick={goToToday}>
-          오늘
-        </button>
-      </span>
+      {/* <span className="rbc-btn-group"></span> */}
       <span className="rbc-btn-group">
         <button type="button" onClick={goToPrev}>
           &lt; 이전 달
         </button>
+        <span className="rbc-toolbar-label">{toolbar.label}</span>
         <button type="button" onClick={goToNext}>
           다음 달&gt;
         </button>
       </span>
-      <span className="rbc-toolbar-label">{toolbar.label}</span>
     </div>
   );
 };
 
-const eachMeal = [
-  "아침: 계란후라이, 토스트",
-  "점심: 김밥",
-  "저녁: 된장찌개, 밥",
-];
-
-const eventData = [
-  {
-    start: new Date(2023, 8, 15),
-    end: new Date(2023, 8, 15),
-    hasDiet: true,
-    dietInfo: eachMeal,
-  },
-];
+// const eventData = [
+//   {
+//     start: new Date(2023, 8, 15),
+//     end: new Date(2023, 8, 15),
+//     hasDiet: true,
+//     dietInfo: {
+//       dailyMealId: 1,
+//       memberId: 4,
+//       date: "2023-09-05",
+//       name: "식단명",
+//       favorite: false,
+//       eachMeals: [
+//         "아침: 계란후라이, 토스트",
+//         "점심: 김밥",
+//         "저녁: 된장찌개, 밥",
+//       ],
+//       totalDailyKcal: 1000,
+//       totalDailyCarbo: 500,
+//       totalDailyProtein: 500,
+//       totalDailyFat: 500,
+//     },
+//   },
+// ];
 
 const CustomCalendar = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [modalContent, setModalContent] = useState(null);
   const [modalHeader, setModalHeader] = useState(null);
   const [modalFooter, setModalFooter] = useState(null);
+  const [eventData, setEventData] = useState([
+    {
+      start: new Date(2023, 8, 15),
+      end: new Date(2023, 8, 15),
+      hasDiet: true,
+      dietInfo: {
+        dailyMealId: 1,
+        memberId: 4,
+        date: "2023-09-05",
+        name: "식단명",
+        favorite: false,
+        eachMeals: [
+          ["아침메뉴: 15일 아침 "],
+          ["점심: 15일 점심"],
+          ["저녁: 15일 저녁 "],
+        ],
+        totalDailyKcal: 1000,
+        totalDailyCarbo: 500,
+        totalDailyProtein: 500,
+        totalDailyFat: 500,
+      },
+    },
+    {
+      start: new Date(2023, 8, 18),
+      end: new Date(2023, 8, 18),
+      hasDiet: true,
+      dietInfo: {
+        dailyMealId: 2,
+        memberId: 5,
+        date: "2023-09-18",
+        name: "식단명",
+        favorite: false,
+        eachMeals: [
+          ["아침메뉴: 어쩌구 18일"],
+          ["점심: 저쩌구 18일 "],
+          ["저녁: 어저구 18일"],
+        ],
+        totalDailyKcal: 1000,
+        totalDailyCarbo: 500,
+        totalDailyProtein: 500,
+        totalDailyFat: 500,
+      },
+    },
+  ]);
+  useEffect(() => {
+    axios
+      .get("/api/")
+      .then((response) => {
+        const modifiedData = response.data.map((item) => {
+          const [year, month, day] = item.date.split("-");
+          const startDate = new Date(year, month - 1, day);
+          const endDate = new Date(year, month - 1, day);
+
+          const eachMealsArray = item.eachMeals.map((meal) => meal.eachMeal);
+
+          return {
+            start: startDate,
+            end: endDate,
+            hasDiet: true,
+            dietInfo: item.eachMeals,
+          };
+        });
+      })
+      .catch((error) => {
+        console.error("fetch error:", error);
+      });
+  }, []);
 
   const handleDateClick = (date, event) => {
     const formattedDate = moment(date).format("YYYY-MM-DD");
@@ -107,14 +175,12 @@ const CustomCalendar = () => {
       const end = moment(event.end).format("YYYY-MM-DD");
       return start === formattedDate || end === formattedDate;
     });
-
-    if (!selectedEvents.length === 0) {
+    if (selectedEvents.length === 0) {
       const content = (
         <div>
-          <h2>선택한 날짜의 식단</h2>
           <ul>
             {selectedEvents.map((event, index) => (
-              <li key={index}>{event.dietInfo}</li>
+              <li key={index}>{event.dietInfo.eachMeals}</li>
             ))}
           </ul>
         </div>
@@ -126,8 +192,8 @@ const CustomCalendar = () => {
 
   const handleDotClick = (event) => {
     if (event.hasDiet) {
-      setModalHeader(<h2>식단 정보</h2>);
-      setModalContent(<div>{event.dietInfo}</div>);
+      setModalHeader(<h2>이 날 식단</h2>);
+      setModalContent(<div>{event.dietInfo.eachMeals}</div>);
       setIsModalOpen(true);
     }
   };
@@ -139,6 +205,7 @@ const CustomCalendar = () => {
   return (
     <CalendarContainer>
       <Calendar
+        style={{ maxWidth: "768px", width: "90%", backgroundColor: "white" }}
         views={["month"]}
         localizer={localizer}
         events={eventData}
@@ -149,6 +216,10 @@ const CustomCalendar = () => {
         components={{
           event: ({ event }) => (event.hasDiet ? <CustomDot /> : null),
           toolbar: customToolbar,
+        }}
+        eventPropGetter={(event, isSelected) => {
+          const backgroundColor = "white";
+          return { style: { backgroundColor } };
         }}
       />
       <Modal
