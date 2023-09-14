1a131f103df659e250064c397fb19c3c0057ca98
사월 <april35592@gmail.com>
2023-09-14 17:16:52 +0900

    devFE-feat/DietPage: 끼니 추가하기

--- Client/src/component/diet/EachMeal.jsx
+++ Client/src/component/diet/EachMeal.jsx
@@ -1,28 +1,56 @@
-import { styled } from "styled-components";
-import GetFood from "./GetFood";
+import Button from "../../atom/button";
+import { PostEachMeal } from "../../util/Diet";
 
-const DivEachMeal = styled.div`
-  background-color: #ffffaa;
-`;
+const EachMeal = ({ meal, timeslot, index, setMeal }) => {
+  const EachMealAddHandler = async (meal, timeslot) => {
+    const result = await PostEachMeal(meal, timeslot);
+    await setMeal(null);
+    await EachMealAddHandler2(result);
+  };
 
-const EachMeal = (howeach, eachmeal) => {
+  const EachMealAddHandler2 = (result) => {
+    setMeal({ ...result });
+  };
+
+  // 1: 아침, 2: 점심, 3: 저녁
+  const timelabel = { 1: "breakfast", 2: "lunch", 3: "dinner" };
+
+  console.log(timeslot);
+  for (let eachMeal of meal.eachMeals) {
+    console.log(eachMeal.timeSlots);
+    if (eachMeal.timeSlots === timeslot) {
+      // dailyMeal에 해당하는 timeslot의 eachMeal이 등록되어있는 경우
+      return (
+        <div className={timelabel[timeslot]}>
+          <h2>{timelabel[timeslot]}</h2>
+          {eachMeal.foods?.map((item, index) => {
+            return (
+              <div key={index}>
+                <p>{item.foodName}</p>
+                <p>{item.quantity}인분</p>
+              </div>
+            );
+          }) ?? null}
+          <Button>음식 추가하기</Button>
+        </div>
+      );
+    }
+  }
+
+  // dailyMeal에 해당하는 timeslot의 eachMeal이 등록되어있지 않는 경우
   return (
-    <DivEachMeal className={howeach}>
-      {eachmeal.eachMealFood.map((eachmealfood) => {
-        const food = GetFood(eachmealfood.foodId);
-        return (
-          <div>
-            <p>{food.foodName}</p>
-            <p>{eachmealfood.quentity}</p>
-          </div>
-        );
-      })}
-      <h3>총 영양소</h3>
-      <p>칼로리: {eachmeal.totalEachKcal}</p>
-      <p>탄수화물: {eachmeal.totalEachCarbo}</p>
-      <p>단백질: {eachmeal.totalEachProtein}</p>
-      <p>지방: {eachmeal.totalEachFat}</p>
-    </DivEachMeal>
+    <div className={timelabel[timeslot]} key={index}>
+      <h2>{timelabel[timeslot]}</h2>
+      <Button
+        onClick={() => {
+          EachMealAddHandler(meal, timeslot);
+        }}
+      >
+        끼니 추가하기
+      </Button>
+      <Button>저장해둔 끼니 불러오기</Button>
+      <Button>끼니 추천하기</Button>
+    </div>
   );
 };
 
---
+++ Client/src/util/Diet.js
@@ -0,0 +1,145 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+const token = localStorage.getItem("Authorization");
+const url = "http://43.201.194.176:8080";
+
+export const GetFoodKeyword = async (value) => {
+  return axios
+    .get(`${url}/search/foods?search-word=${value}`, {
+      headers: { Authorization: token },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    });
+};
+
+export const PostEachMeal = async (meal, timeslot) => {
+  return axios
+    .post(
+      `${url}/eachmeals`,
+      {
+        dailymealId: meal.dailymealsId,
+        timeSlots: timeslot,
+        foods: [],
+      },
+      {
+        headers: {
+          Authorization: token,
+        },
+      }
+    )
+    .then(async (response) => {
+      // 성공한 경우 실행
+      console.log(response);
+
+      return axios
+        .patch(
+          `${url}/dailymeals/date/${meal.date}`,
+          {
+            name: "name",
+            eachMeals: [
+              ...meal.eachMeals.map((item) => item.eachMealId),
+              response.data.eachMealId,
+            ],
+          },
+          {
+            headers: {
+              Authorization: token,
+              // "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          console.log(response);
+          return response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      // 에러인 경우 실행
+      console.log(error);
+    });
+};
+
+export const GetFood = (foodId) => {
+  const [food, setFood] = useState();
+  axios
+    .get(`${url}/foods/${foodId}`, {
+      headers: { Authorization: token },
+    })
+    .then((response) => {
+      // 성공한 경우 실행
+      console.log(response);
+      setFood(() => {
+        return response.data;
+      });
+    })
+    .catch((error) => {
+      // 에러인 경우 실행
+      console.log(error);
+    });
+  return food;
+};
+
+const today = new Date();
+let year = today.getFullYear(); // 년도
+let month = today.getMonth() + 1; // 월
+let date = today.getDate(); //일
+
+export const GetDailyDiet = (
+  dateStr = `${year}-${month >= 10 ? month : "0" + month}-${
+    date >= 10 ? date : "0" + date
+  }`
+) => {
+  const url1 = `${url}/dailymeals`;
+  const url2 = `${url}/dailymeals/date/${dateStr}`;
+  console.log(url2);
+  const [meal, setMeal] = useState();
+
+  useEffect(() => {
+    axios
+      .get(url2, {
+        headers: { Authorization: token },
+      })
+      .then((response) => {
+        console.log(response);
+        setMeal(() => response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        //불러오기 실패시 post요청으로 dailymeals 생성
+        if (error.response.data === "DailyMeal not found...") {
+          axios
+            .post(
+              url1,
+              {
+                name: "name",
+                date: dateStr,
+                favorite: false,
+                eachMeals: [],
+              },
+              {
+                headers: {
+                  Authorization: token,
+                },
+              }
+            )
+            .then((response) => {
+              console.log(response);
+              setMeal(() => {
+                setMeal(response.data);
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
+  }, [dateStr, url1, url2]);
+
+  return meal;
+};
