89c7b3d60189493c4256cbc5974472ba9114d6d9
Rinn202 <dioneo54@gmail.com>
2023-09-15 12:27:05 +0900

    .

---
+++ main_project/src/main/java/NutrientsCoders/main_project/dailymeal/dto/PagedResponse.java
@@ -0,0 +1,18 @@
+package NutrientsCoders.main_project.dailymeal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagedResponse<T> {
+  private List<T> content;
+  private long totalElements;
+  private int totalPages;
+  private boolean last;
+  
+}
