755ac0b99e8eb3de0c1c7c96a92dcfe8e386475b
사월 <april35592@gmail.com>
2023-09-19 12:42:45 +0900

    devFE-style/whole: Header, navbar

--- Client/src/component/Nav.jsx
+++ Client/src/component/Nav.jsx
@@ -1,79 +1,73 @@
 import { styled } from "styled-components";
 import style from "../style/style";
 import { Link } from "react-router-dom";
+import navHomeIcon from "../asset/navHomeIcon.png";
+import navCommunityIcon from "../asset/navCommunityIcon.png";
+import navMyPageIcon from "../asset/navMyPageIcon.png";
 
 const NavContainer = styled.nav`
   position: absolute;
-  top: ${style.layout.header.height + style.layout.wideMargin.height};
-  left: ${style.layout.sideMargin/6};
+  top: 80px;
+  left: 56px;
 `;
 const UlBox = styled.ul`
   list-style: none;
-  &>li{
-    width: ${style.layout.sideMargin/3};
-    margin-bottom: ${style.layout.wideMargin.height};
+  & > li {
+    width: 48px;
+    margin-bottom: 20px;
     overflow: hidden;
     white-space: nowrap;
     transition: 1s;
     background-color: orange;
-    border-radius: 10px;
-    &>a{
+    border-radius: 8px;
+    font-size: 16px;
+    font-weight: 600;
+    box-shadow: 0 2px 4px 1px rgba(0, 0, 0, 0.2);
+    & > a {
       display: flex;
       align-items: center;
     }
-    &>a>:first-child{
-      margin: 0; padding: 0;
-      background-color: white;
-      width: ${style.layout.sideMargin/3};
-      height: ${style.layout.sideMargin/3};
-      border-radius: 10px;
+    & > a > img {
+      margin: 0;
+      padding: 0;
+      width: 48px;
+      height: 48px;
+      border-radius: 8px;
     }
-    &>a>:last-child{
-      padding-left: ${style.layout.narrowMargin.width/3};
+    & > a > :last-child {
+      padding-left: 10px;
     }
     &:hover {
-      width: ${style.layout.sideMargin*4/5};
+      width: 160px;
       transition: 1s;
     }
   }
 `;
 
-function Nav({setPage}) {
+const Nav = () => {
   return (
     <NavContainer>
       <UlBox>
         <li>
-          <Link to="/pageswitch/community">
-            <img src="https://media.discordapp.net/attachments/1144143589740400680/1146772585284116530/Frame_1.png?width=116&height=116"></img>
-            <div>검색페이지</div>
-          </Link>
-        </li>
-        <li>
           <Link to="/">
-            <img src="https://media.discordapp.net/attachments/1144143589740400680/1146772585548349542/Frame_2.png?width=116&height=116"></img>
+            <img src={navHomeIcon} alt="home icon" />
             <div>홈 화면</div>
           </Link>
         </li>
         <li>
-          <Link to="/pageswitch/mypage">
-            <img src="https://media.discordapp.net/attachments/1144143589740400680/1146772585787445348/Frame_3.png?width=116&height=116"></img>
-            <div>마이페이지</div>
-          </Link>
-        </li>
-        <li>
           <Link to="/pageswitch/community">
-            <img src="https://media.discordapp.net/attachments/1144143589740400680/1146772586051674152/Frame_4.png?width=116&height=116"></img>
+            <img src={navCommunityIcon} alt="community icon" />
             <div>커뮤니티</div>
           </Link>
         </li>
         <li>
-          <Link to="/pageswitch/chat">
-            <img src="https://media.discordapp.net/attachments/1144143589740400680/1146772586496262275/Frame_5.png?width=116&height=116"></img>
-            <div>뉴코채팅</div>
+          <Link to="/pageswitch/mypage">
+            <img src={navMyPageIcon} alt="community icon" />
+            <div>마이페이지</div>
           </Link>
         </li>
       </UlBox>
     </NavContainer>
   );
-}
+};
 export default Nav;
